51fd6b232ecdb7e6e86591623e6a0a8bdbcfa97c
Adrian Coral <adriancoral@gmail.com>
2022-12-04 19:57:33 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Demo GP
+
+API demo GP en Laravel 9.19, PHP 8.1
+
+## Inicio
---
+++ src/app/Traits/ApiResponses.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
+trait ApiResponser
+{
+    /**
+     * @param $data
+     * @param array $headers
+     * @param int $options
+     * @return JsonResponse
+     */
+    protected function successfulResponse($data, array $headers = [], int $options = 0): JsonResponse
+    {
+        return response()->json(['success' => true, 'payload' => $data], 200, [], JSON_NUMERIC_CHECK);
+    }
+
+    /**
+     * @param $message
+     * @param $code
+     *
+     * @return JsonResponse
+     */
+    protected function errorResponse($message, $code): JsonResponse
+    {
+        Log::warning("HTTP ERROR ${code}", ['message' => $message]);
+        return response()->json(['success' => false, 'message' => $message], $code);
+    }
+}
